5468202cc7f2bbcea40f7994737f7ecc6e7ee962
navruzov-lang <navruzov1609@gmail.com>
2026-03-02 03:06:02 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,4 +24,20 @@ app.add_handler(CommandHandler("start", start))
 
 print("Test Bot ishga tushdi 🚀")
 
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+threading.Thread(target=run_web).start()
+
 app.run_polling()
